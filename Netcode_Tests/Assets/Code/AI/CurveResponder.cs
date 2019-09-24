120875c370ef49531c3e4417e1104f486fe11cd4
Teppich <johannesspicker@me.com>
2019-09-25 00:06:33 +0200

    [PRO] starting ai prototype
    no substantial code yet

---
+++ Netcode_Tests/Assets/Code/AI/CurveResponder.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CurveResponder
+{
+    public enum CurveType { Linear, Quadratic, Logistic, Logit };
+
+    //takes function calls:
+    //(x, CurveType, m, k, b, c)
+    //Clamping input (0..1)
+    //Calc using appropriate formula type
+    //Clamp output (0..1)
+    //return y
+
+    public static float RespondCurve(float x, CurveType type, float m, float k, float b, float c)
+    {
+        switch (type)
+        {
+            case CurveType.Linear:
+                return 1;
+            case CurveType.Quadratic:
+                return 1;
+            case CurveType.Logistic:
+                return 1;
+            case CurveType.Logit:
+                return 1;
+            default:
+                return 0;
+        }
+    }
+}
---
+++ Netcode_Tests/Assets/Code/AI/PawnController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PawnController : MonoBehaviour
+{
+    private NavMeshAgent navMeshAgent;
+    [SerializeField] private Transform followThis;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMeshAgent.SetDestination(followThis.position);        
+    }
+}
