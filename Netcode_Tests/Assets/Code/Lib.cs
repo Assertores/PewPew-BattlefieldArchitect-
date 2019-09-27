136f3ec9e5f989dfc7cf9e57e866e35f247c57fc
[JAM] Andreas <andreas.edmeier@web.de>
2019-09-27 23:29:37 +0200

    [PRO] created Lib

---
+++ Netcode_Tests/Assets/Code/Lib.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Lib {
+	public static float mod(float a, float b) {
+		return a - b * Mathf.Floor(a / b);
+	}
+
+	public static int mod(int a, int b) {
+		return a - b * Mathf.FloorToInt((float)a / b);
+	}
+}
