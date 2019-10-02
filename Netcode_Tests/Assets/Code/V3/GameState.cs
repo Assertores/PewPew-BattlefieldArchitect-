d7d6188940cb1a6de5c60fe3826307600b3086af
[JAM] Andreas <andreas.edmeier@web.de>
2019-10-02 21:26:40 +0200

    [PRO] filled gamestate with variables

--- Netcode_Tests/Assets/Code/V3/GameState.cs
+++ Netcode_Tests/Assets/Code/V3/GameState.cs
@@ -3,9 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace NT3 {
+
+	public struct GSI_Pos {
+		public int m_id;
+		public float m_x;
+		public float m_y;
+		public float m_z;
+	}
+	public struct GSI_Health {
+		public int m_id;
+		public float m_health;
+	}
+	public struct GSI_Arg {
+		public int m_id;
+		public byte m_arg;
+	}
+
 	public class GameState {
 
-		public int RefTick = -1;
+		public int m_refTick = -1;
+		public List<GSI_Pos> m_positions = new List<GSI_Pos>();
+		public List<GSI_Health> m_healths = new List<GSI_Health>();
+		public List<GSI_Arg> m_arguments = new List<GSI_Arg>();
+
+		bool m_isLerped;
+		bool m_isDelta;
 		public byte[] Encrypt() {
 			return null;
 		}
