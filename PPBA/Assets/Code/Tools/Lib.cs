1ffbbd4f51f202429949981d111f449e6910705a
[JAM] Andreas <andreas.edmeier@web.de>
2019-10-25 16:30:03 +0200

    [PRO] added static Lib class with fixed Mod funktion

---
+++ PPBA/Assets/Code/Tools/Lib.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PPBA
+{
+	public static class Lib
+	{
+		public static float Mod(float a, float b)
+		{
+			return a - b * Mathf.Floor(a / b);
+		}
+
+		public static int Mod(int a, int b)
+		{
+			return a - b * Mathf.FloorToInt((float)a / b);
+		}
+	}
+}
