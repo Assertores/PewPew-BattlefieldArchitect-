f7dc4ecf0cd1579a32899cb8951297b89134a3d5
[JAM] Andreas <andreas.edmeier@web.de>
2019-11-04 16:34:36 +0100

    [PRO] TickHandler Interface implimented

---
+++ PPBA/Assets/Code/Network/TickHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace PPBA {
+	public class TickHandler : Singleton<TickHandler>
+	{
+		public static Action<int> s_DoInput;
+		public static Action<int> s_EarlyCalc;
+		public static Action<int> s_LateCalc;
+		public static Action<int> s_DoTick;
+		public static Action<int> s_GatherValues;
+
+		private int _currentTick;
+	}
+}
