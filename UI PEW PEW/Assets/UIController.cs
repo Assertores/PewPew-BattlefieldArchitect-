df410f20383f8d165a48a6620b5049497a873d63
Rene <Rene.Gnahs@gmx.de>
2019-11-05 02:37:59 +0100

    Add Canvas with BuildPanel , add UnitScreen with LivePicture
    healthbar Ammo with litthebit Functions in background

--- UI PEW PEW/Assets/UIController.cs
+++ UI PEW PEW/Assets/UIController.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 using DG.Tweening;
 
 
@@ -10,69 +7,93 @@ public class UIController : MonoBehaviour
 	public RectTransform MainBuildPanel;
 	public RectTransform buildUI_1;
 	public RectTransform buildUI_2;
-	public float PanelHeight = 300;
 
 	private bool _isOpen1 = false;
 	private bool _isOpen2 = false;
 	private bool _isMainPanel = false;
 
 
-	public void MoveBuildsPanel(RectTransform rec, bool status)
+	public void MoveMainPanel()
+	{
+		if (_isMainPanel)
+		{
+			MainBuildPanel.DOAnchorPos(new Vector2(-MainBuildPanel.rect.x, 120), 0.25f);
+		}
+		else
+		{
+			MainBuildPanel.DOAnchorPos(new Vector2(-MainBuildPanel.rect.x, 320), 0.25f);
+		}
+
+		_isMainPanel = !_isMainPanel;
+	}
+
+	public void MoveBuild1Panel()
 	{
 		if (!_isMainPanel)
 		{
-			SwitchBuildPanel(MainBuildPanel, !_isMainPanel);
-			_isMainPanel = !_isMainPanel;
+			MoveMainPanel();
 		}
 
-		SwitchBuildPanel(rec, status);
-	}
-
-	public void SwitchBuildPanel(RectTransform rec, bool dir)
-	{
-		if (dir)
-		{
-			rec.DOAnchorPos(new Vector2(0, PanelHeight), 0.25f);
-		}
-		else
-		{
-			rec.DOAnchorPos(new Vector2(0, 0), 0.25f);
-		}
-
-	}
-
-	public void CloseBuildPanels()
-	{
-		SwitchBuildPanel(MainBuildPanel, false);
-		SwitchBuildPanel(buildUI_1, false);
-		SwitchBuildPanel(buildUI_2, false);
-	}
-
-	public void SwitchBuild1()
-	{
 		if (_isOpen1)
 		{
-			MoveBuildsPanel(buildUI_1, !_isOpen1);
-			_isOpen1 = !_isOpen1;
+			buildUI_1.DOAnchorPos(new Vector2(0, -350), 0.25f);
 		}
 		else
 		{
-			CloseBuildPanels();
+			buildUI_1.DOAnchorPos(new Vector2(0, 0), 0.25f);
+			if (_isOpen2)
+			{
+				buildUI_2.DOAnchorPos(new Vector2(0, -350), 0.25f);
+				_isOpen2 = !_isOpen2;
+			}
+		}
+
+		_isOpen1 = !_isOpen1;
+
+		if (!_isOpen1 && !_isOpen2)
+		{
+			MoveMainPanel();
 		}
 	}
 
-	public void SwitchBuild2()
+
+	public void MoveBuild2Panel()
 	{
+		if (!_isMainPanel)
+		{
+			MoveMainPanel();
+		}
+
 		if (_isOpen2)
 		{
-			MoveBuildsPanel(buildUI_1, !_isOpen2);
-			_isOpen2 = !_isOpen2;
+			buildUI_2.DOAnchorPos(new Vector2(0, -350), 0.25f);
 		}
 		else
 		{
-			CloseBuildPanels();
+			buildUI_2.DOAnchorPos(new Vector2(0, 0), 0.25f);
+			if (_isOpen1)
+			{
+				buildUI_1.DOAnchorPos(new Vector2(0, -350), 0.25f);
+				_isOpen1 = !_isOpen1;
+
+			}
 		}
 
+		_isOpen2 = !_isOpen2;
+
+		if (!_isOpen1 && !_isOpen2)
+		{
+			MoveMainPanel();
+		}
 
 	}
+
+	private void CloseAll()
+	{
+		buildUI_1.DOAnchorPos(new Vector2(0, 0), 0.25f);
+		buildUI_2.DOAnchorPos(new Vector2(0, 0), 0.25f);
+		MainBuildPanel.DOAnchorPos(new Vector2(-MainBuildPanel.rect.x, 320), 0.25f);
+
+	}
+
 }
