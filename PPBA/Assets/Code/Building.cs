b4ccfe0d73eeade620937af9aad030204d35a4f8
[JAM] Andreas <andreas.edmeier@web.de>
2019-11-06 11:24:43 +0100

    Example AABB Select

---
+++ PPBA/Assets/Code/Building.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PPBA
+{
+	public class Building : MonoBehaviour
+	{
+		public static List<Building> s_refs { get; private set; } = new List<Building>();
+
+		private void Start()
+		{
+			s_refs.Add(this);
+		}
+
+		private void OnDestroy()
+		{
+			s_refs.Remove(this);
+		}
+
+		public bool Select(Rect aabb)
+		{
+			Vector2 pos = Camera.main.WorldToScreenPoint(transform.position);
+
+			if(pos.x > aabb.xMin && pos.x < aabb.xMax && pos.y > aabb.yMin && pos.y < aabb.yMax)
+			{
+				print(gameObject.name);
+				return true;
+			}
+
+			return false;
+		}
+	}
+}
---
+++ PPBA/Assets/Code/SelectInput.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PPBA {
+	public class SelectInput : MonoBehaviour
+	{
+		private Vector2 _starPos;
+		void Update()
+		{
+			if(Input.GetMouseButtonDown(0))
+			{
+				_starPos = Input.mousePosition;
+			} else if(Input.GetMouseButtonUp(0))
+			{
+				Rect aabb = new Rect();
+				aabb.xMin = Mathf.Min(_starPos.x, Input.mousePosition.x);
+				aabb.xMax = Mathf.Max(_starPos.x, Input.mousePosition.x);
+				aabb.yMin = Mathf.Min(_starPos.y, Input.mousePosition.y);
+				aabb.yMax = Mathf.Max(_starPos.y, Input.mousePosition.y);
+
+				foreach(var it in Building.s_refs)
+				{
+					it.Select(aabb);
+				}
+			}
+		}
+	}
+}
