9d4148534f53bedb085c4490922b39b6fa349e5d
Rene <Rene.Gnahs@gmx.de>
2019-11-06 13:24:47 +0100

    add UserInterfaceController

---
+++ PPBA/Assets/Code/Tools/UserInputController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PPBA
+{
+	public class UserInputController : Singleton<UserInputController>
+	{
+
+
+		// Start is called before the first frame update
+		void Start()
+		{
+
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+	}
+}
