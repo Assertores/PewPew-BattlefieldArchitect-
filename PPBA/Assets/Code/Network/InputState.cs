93d2da2d140114967b70f41c05553025d8fd1495
[JAM] Andreas <andreas.edmeier@web.de>
2019-11-07 14:48:57 +0100

    [PRO] added En- and Decrypting funktions

--- PPBA/Assets/Code/Network/InputState.cs
+++ PPBA/Assets/Code/Network/InputState.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 namespace PPBA
 {
-	public enum ObjectType { NON, REFINARY, WALL, SIZE}
+	public enum ObjectType : byte { NON, REFINARY, WALL, SIZE}
 
 	namespace ISC //Input State Component
 	{
@@ -66,6 +67,116 @@ namespace PPBA
 			value._corners = corners.ToArray();
 			_combinedObjs.Add(value);
 			return value._id;
+		}
+
+		/// <summary>
+		/// Encrypts the data to sendable byte array
+		/// </summary>
+		/// <returns>the message</returns>
+		public byte[] Encrypt()
+		{
+			List<byte> value = new List<byte>();
+
+			value.AddRange(BitConverter.GetBytes(_objs.Count));
+			foreach(var it in _objs)
+			{
+				value.AddRange(BitConverter.GetBytes(it._id));
+				value.Add((byte)it._type);
+				value.AddRange(BitConverter.GetBytes(it._pos.x));
+				value.AddRange(BitConverter.GetBytes(it._pos.y));
+				value.AddRange(BitConverter.GetBytes(it._pos.z));
+				value.AddRange(BitConverter.GetBytes(it._angle));
+			}
+
+			value.AddRange(BitConverter.GetBytes(_combinedObjs.Count));
+			foreach(var it in _combinedObjs)
+			{
+				value.AddRange(BitConverter.GetBytes(it._id));
+				value.Add((byte)it._type);
+				value.AddRange(BitConverter.GetBytes(it._corners.Length));
+				for(int i = 0; i < it._corners.Length; i++)
+				{
+					value.AddRange(BitConverter.GetBytes(it._corners[i].x));
+					value.AddRange(BitConverter.GetBytes(it._corners[i].y));
+					value.AddRange(BitConverter.GetBytes(it._corners[i].z));
+				}
+			}
+
+			return value.ToArray();
+		}
+
+		/// <summary>
+		/// Decypts an byte-array to usable data
+		/// </summary>
+		/// <param name="msg">the byte-array</param>
+		/// <param name="offset">the start offset</param>
+		/// <returns>the offset after the point it stoped reading</returns>
+		public int Decrypt(int client, byte[] msg, int offset)
+		{
+			int count = BitConverter.ToInt32(msg, offset);
+			offset += sizeof(int);
+
+			for(int i = 0; i < count; i++)
+			{
+				ISC.obj tmp = new ISC.obj();
+
+				tmp._id = BitConverter.ToInt32(msg, offset);
+				offset += sizeof(int);
+
+				tmp._client = client;
+
+				tmp._type = (ObjectType)msg[offset];
+				offset++;
+
+				tmp._pos.x = BitConverter.ToSingle(msg, offset);
+				offset += sizeof(float);
+
+				tmp._pos.y = BitConverter.ToSingle(msg, offset);
+				offset += sizeof(float);
+
+				tmp._pos.z = BitConverter.ToSingle(msg, offset);
+				offset += sizeof(float);
+
+				tmp._angle = BitConverter.ToSingle(msg, offset);
+				offset += sizeof(float);
+
+				_objs.Add(tmp);
+			}
+
+			count = BitConverter.ToInt32(msg, offset);
+			offset += sizeof(int);
+
+			for(int i = 0; i < count; i++)
+			{
+				ISC.combinedObj tmp = new ISC.combinedObj();
+
+				tmp._id = BitConverter.ToInt32(msg, offset);
+				offset += sizeof(int);
+
+				tmp._client = client;
+
+				tmp._type = (ObjectType)msg[offset];
+				offset++;
+
+				tmp._corners = new Vector3[BitConverter.ToInt32(msg, offset)];
+				offset += sizeof(int);
+
+				for(int j = 0; j < tmp._corners.Length; j++)
+				{
+					tmp._corners[j].x = BitConverter.ToInt32(msg, offset);
+					offset += sizeof(int);
+
+					tmp._corners[j].y = BitConverter.ToInt32(msg, offset);
+					offset += sizeof(int);
+
+					tmp._corners[j].z = BitConverter.ToInt32(msg, offset);
+					offset += sizeof(int);
+				}
+
+				_combinedObjs.Add(tmp);
+			}
+
+			return offset;
 		}
 	}
 }
