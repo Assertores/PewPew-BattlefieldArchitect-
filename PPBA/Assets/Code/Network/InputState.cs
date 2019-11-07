6c72564aeb5dca6b6995d833dd568ba2cce989ae
[JAM] Andreas <andreas.edmeier@web.de>
2019-11-07 14:24:19 +0100

    [PRO] implimented Add funktions

--- PPBA/Assets/Code/Network/InputState.cs
+++ PPBA/Assets/Code/Network/InputState.cs
@@ -23,17 +23,19 @@ namespace PPBA
 
 		public class combinedObj : isc
 		{
-			public List<Vector3> _corners;
+			public Vector3[] _corners;
 		}
 	}
 
 	public class InputState : MonoBehaviour
 	{
+		private static int _currentID = 0;
+
 		public List<ISC.obj> _objs = new List<ISC.obj>();
 		public List<ISC.combinedObj> _combinedObjs = new List<ISC.combinedObj>();
 
 		/// <summary>
-		/// 
+		/// adds a new object to the InputState
 		/// </summary>
 		/// <param name="type">type of the placed object</param>
 		/// <param name="pos">position of the placed object</param>
@@ -41,18 +43,29 @@ namespace PPBA
 		/// <returns>input identifyer</returns>
 		public int AddObj(ObjectType type, Vector3 pos, float angle)
 		{
-			return -1;
+			ISC.obj value = new ISC.obj();
+			value._id = _currentID++;
+			value._type = type;
+			value._pos = pos;
+			value._angle = angle;
+			_objs.Add(value);
+			return value._id;
 		}
 
 		/// <summary>
-		/// 
+		/// adds a wall or streat or trench or comparable to the InputState
 		/// </summary>
 		/// <param name="type">type of the placed object</param>
 		/// <param name="corners">list of corner positions</param>
 		/// <returns>input identifyer</returns>
 		public int AddCombinedObj(ObjectType type, List<Vector3> corners)
 		{
-			return -1;
+			ISC.combinedObj value = new ISC.combinedObj();
+			value._id = _currentID++;
+			value._type = type;
+			value._corners = corners.ToArray();
+			_combinedObjs.Add(value);
+			return value._id;
 		}
 	}
 }
