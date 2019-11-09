28da6fca329fa0294ea46d126374046b10d22f5c
Rene <Rene.Gnahs@gmx.de>
2019-11-09 02:07:44 +0100

    [PRO] fix Mat , add GhostMAt for GhostBuild

--- PPBA/Assets/Code/CameraController.cs
+++ PPBA/Assets/Code/CameraController.cs
@@ -10,43 +10,40 @@ public class CameraController : MonoBehaviour
 	[SerializeField] float _CameraSpeed = 20f;
 	[SerializeField] float _RotateSpeed = 20f;
 	[SerializeField] float _BorderThickness = 10f;
-	[SerializeField] Vector2 _panLimit = new Vector2(40f,40f);
+	[SerializeField] Vector2 _panLimit = new Vector2(40f, 40f);
 
 	void Update()
 	{
 		if(Input.GetMouseButton(1))
 		{
-			 (new Vector3(0, Input.GetAxis("Mouse X") * _RotateSpeed, 0), Space.World);
-			transform.rotation = Quaternion.Euler(0,,0);
+			//(new Vector3(0, Input.GetAxis("Mouse X") * _RotateSpeed, 0), Space.World);
+			transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X") * _RotateSpeed, 0), Space.World);
 		}
 
-		Vector3 pos = transform.position;
+		Vector3 pos = Vector3.zero;
 		Vector3 mousePos = Input.mousePosition;
 
 		if(Input.GetKey(KeyCode.W) || mousePos.y >= Screen.height - _BorderThickness)
 		{
-			pos.z += _CameraSpeed * Time.deltaTime;
+			pos += _CameraSpeed * Vector3.forward * Time.deltaTime;
 		}
 		if(Input.GetKey(KeyCode.S) || mousePos.y <= _BorderThickness)
 		{
-			pos.z -= _CameraSpeed * Time.deltaTime;
+			pos += _CameraSpeed * Vector3.back * Time.deltaTime;
 		}
 		if(Input.GetKey(KeyCode.D) || mousePos.x >= Screen.width - _BorderThickness)
 		{
-			pos.x += _CameraSpeed * Time.deltaTime;
+			pos += _CameraSpeed * Vector3.right * Time.deltaTime;
 		}
 		if(Input.GetKey(KeyCode.A) || mousePos.x <= _BorderThickness)
 		{
-			pos.x -= _CameraSpeed * Time.deltaTime;
+			pos += _CameraSpeed * Vector3.left * Time.deltaTime;
 		}
 
-		pos.x = Mathf.Clamp(pos.x, -_panLimit.x, _panLimit.x);
-		pos.z = Mathf.Clamp(pos.z, -_panLimit.y, _panLimit.y);
+		pos.x = Mathf.Clamp(transform.position.x, -_panLimit.x, _panLimit.x);
+		pos.z = Mathf.Clamp(transform.position.z, -_panLimit.y, _panLimit.y);
+		transform.Translate(pos, Space.Self);
 
-		Vector3 camF = Vector3.forward;
-		Vector3 camR = Vector3.right;
-
-		transform.position = (camF*pos.x+ camR*pos.y;
 	}
 
 }
