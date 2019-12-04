7f047ae9b3687716a575ade6ec4c711026189f53
Rene <Rene.Gnahs@gmx.de>
2019-12-04 21:42:42 +0100

    [PRO] new Folder for Shader, test movescript (territorium)

--- PPBA/Assets/Code/Shader/TerritoriumMapCalculate.cs
+++ PPBA/Assets/Code/Shader/TerritoriumMapCalculate.cs
@@ -9,7 +9,7 @@ namespace PPBA
 		public ComputeShader _computeShader;
 		public Material _GroundMaterial;
 
-		private RenderTexture TerritorriumMap;
+		public Texture TerritorriumMap;
 		private RenderTexture _ResultTexture;
 		private ComputeBuffer _buffer;
 
@@ -17,9 +17,8 @@ namespace PPBA
 		private int _resourceCalcKernel2;
 		private List<RefineryRefHolder> _Refinerys = new List<RefineryRefHolder>();
 
+		private List<Vector4> _Soldiers = new List<Vector4>();
 
-		[SerializeField]
-		private int[] _ResourceValues;
 
 		void Start()
 		{
@@ -32,23 +31,22 @@ namespace PPBA
 			Texture2D resourceTexture = Instantiate(_GroundMaterial.GetTexture("_TerritorriumMap")) as Texture2D;
 			_resourceCalcKernel = _computeShader.FindKernel("CSTerritorium");
 
-			_ResultTexture = new RenderTexture(resourceTexture.width, resourceTexture.height, 0, RenderTextureFormat.R8)
+			_ResultTexture = new RenderTexture(resourceTexture.width, resourceTexture.height, 0, RenderTextureFormat.ARGB32)
 			{
 				enableRandomWrite = true
 			};
 
 			_ResultTexture.Create();
 			Graphics.Blit(resourceTexture, _ResultTexture);
-			Graphics.Blit(resourceTexture, TerritorriumMap);
 
 			_GroundMaterial.SetTexture("_TerritorriumMap", _ResultTexture);
 		}
 
-
-
-		public void RefreshCalcRes()
+		public void RefreshCalcTerritorium()
 		{
+			_computeShader.SetInt("SoldiersSize", _Soldiers.Count);
 			_computeShader.SetTexture(_resourceCalcKernel, "TerritoriumResult", _ResultTexture);
+			_computeShader.SetVectorArray("coords", AddSoldier());
 
 			_computeShader.Dispatch(_resourceCalcKernel, 512 / 8, 512 / 8, 1);
 
@@ -61,27 +59,37 @@ namespace PPBA
 			//	ressourceManager.AddRessourcesToRefineries(CurrentValue);
 		}
 
-		private Vector4[] RefineriesProperties()
+		private Vector4[] AddSoldier()
 		{
-			Vector4[] refsProp = new Vector4[_Refinerys.Count];
+			Vector4[] refsProp = new Vector4[_Soldiers.Count];
 
-			for(int i = 0; i < _Refinerys.Count; i++)
+			for(int i = 0; i < _Soldiers.Count; i++)
 			{
-				refsProp[i] = _Refinerys[i].GetShaderProperties();
+				refsProp[i] = new Vector4(_Soldiers[i].x , _Soldiers[i].y, _Soldiers[i].z, 0);
 			}
 			return refsProp;
 		}
 
-		void Update()
+
+		public int AddSoldier(Transform sol, Vector2 pos, int Team)
 		{
-			if(Input.GetKeyDown(KeyCode.M))
-			{
-				Vector4[] test = new Vector4[1];
-				test[0] = new Vector4(100.0f, 100.0f, 1.0f);
-				_computeShader.SetVectorArray("Soldiers", test);
-				RefreshCalcRes();
-			}
+
+			_Soldiers.Add(new Vector4(pos.x , pos.y, Team ));
+			return _Soldiers.Count-1;
 		}
+
+
+		//void Update()
+		//{
+		//	if(Input.GetKeyDown(KeyCode.M))
+		//	{
+		//		Vector4[] test = new Vector4[2];
+		//		test[0] = new Vector4(100.0f, 100.0f, 1.0f, 0);
+		//		test[1] = new Vector4(256.0f, 256.0f, 0.0f, 0);
+		//		_computeShader.SetVectorArray("Soldiers", test);
+		//		RefreshCalcTerritorium();
+		//	}
+		//}
 
 		void OnDisable()
 		{
---
+++ PPBA/Assets/movetest.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PPBA;
+
+public class movetest : MonoBehaviour
+{
+	public int team;
+
+	public Transform pos1;
+	public Transform pos2;
+	private Transform target;
+	public float speed = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		target = pos1;
+		Vector2 pos = UserInputController.s_instance.GetTexturePixelPoint();
+		TerritoriumMapCalculate.s_instance.AddSoldier(this.transform, pos, 0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		float dis = Vector3.Distance(target.position, transform.position);
+
+		if(dis <= 0.2f)
+		{
+			if(target == pos1 )
+			{
+				target = pos2;
+			}
+			else
+			{
+				target = pos1;
+			}
+		}
+
+		transform.position = Vector3.MoveTowards(transform.position, target.position, speed* Time.deltaTime);
+		   		 
+    }
+
+	IEnumerator calc()
+	{
+		yield return new WaitForSeconds(1);
+
+	}
+
+
+}
