4b6a871924cd0fca716294d03708c5e9e18cc28f
[JAM] Andreas <andreas.edmeier@web.de>
2019-12-06 15:57:56 +0100

    [PRO] added Log GameState on server and client

--- PPBA/Assets/Code/Network/TickHandler.cs
+++ PPBA/Assets/Code/Network/TickHandler.cs
@@ -97,6 +97,8 @@ namespace PPBA
 
 			s_GatherValues?.Invoke(s_currentTick);
 
+			Debug.Log("Tick: " + s_currentTick + "\n" + s_interfaceGameState.ToString());
+
 			//Debug.Log("[Server] Seperating Gamestate");
 			foreach(var it in GlobalVariables.s_instance._clients)
 			{
@@ -185,6 +187,8 @@ namespace PPBA
 			s_interfaceInputState = me._inputStates[nextStateTick];
 
 			s_currentTickTime = Time.time;
+
+			Debug.Log("Tick: " + s_currentTick + "\n" + s_interfaceGameState.ToString());
 
 			s_SetUp?.Invoke(s_currentTick);
 			s_DoInput?.Invoke(s_currentTick);
