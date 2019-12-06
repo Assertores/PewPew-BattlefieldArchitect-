0b95c341322480070f8e53424a9617515d9a172e
[JAM] Andreas <andreas.edmeier@web.de>
2019-12-06 11:54:58 +0100

    [PRO] created HeatMap to GameState script

---
+++ PPBA/Assets/Code/HeatMap/HeatMapToGameStateConverter.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PPBA
+{
+	public class HeatMapToGameStateConverter : MonoBehaviour
+	{
+		void Start()
+		{
+#if UNITY_SERVER
+			TickHandler.s_GatherValues += Converter;
+#endif
+		}
+		private void OnDestroy()
+		{
+#if UNITY_SERVER
+			TickHandler.s_GatherValues -= Converter;
+#endif
+		}
+
+		void Converter(int tick)
+		{
+			foreach(var it in HeatMapHandler.s_instance.GetAllIds())
+			{
+				GSC.heatMap hm = new GSC.heatMap();
+				hm._id = it;
+
+				//setting up current Bitfield
+				{
+					Vector2Int size = HeatMapHandler.s_instance.GetHeatMapSize(it);
+					hm._mask = new BitField2D(size.x, size.y);
+					Vector2Int[] unordertPositions = HeatMapHandler.s_instance.GetChangedPositions(it);
+					foreach(var jt in unordertPositions)
+					{
+						hm._mask[jt.x, jt.y] = true;
+					}
+				}
+				Vector2Int[] positions = hm._mask.GetActiveBits();
+
+				//saving values
+				Texture2D heatMap = HeatMapHandler.s_instance.GetHeatMap(it);
+
+				hm._values = new List<float>(positions.Length);
+				for(int i = 0; i < positions.Length; i++)
+				{
+					hm._values.Add(heatMap.GetPixel(positions[i].x, positions[i].y).r);
+				}
+
+				TickHandler.s_interfaceGameState._heatMaps.Add(hm);
+			}
+		}
+	}
+}
