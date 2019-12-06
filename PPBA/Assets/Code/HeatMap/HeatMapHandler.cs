b5ec8affe884c89fdb801d1b5bb98473eceeb2e9
[JAM] Andreas <andreas.edmeier@web.de>
2019-12-06 14:55:07 +0100

    [PRO] moved calculations to internal referencing

--- PPBA/Assets/Code/HeatMap/HeatMapHandler.cs
+++ PPBA/Assets/Code/HeatMap/HeatMapHandler.cs
@@ -9,57 +9,61 @@ namespace PPBA
 		public RenderTexture tex;
 		public byte[] bitfield;
 	}
+
 	public class HeatMapHandler : Singleton<HeatMapHandler>
 	{
-		RenderTexture[] _heatMaps = new RenderTexture[2];
+		Texture2D[] _heatMaps = new Texture2D[2];
 		BitField2D[] _bitFields = new BitField2D[2];
 
 		private void Start()
 		{
 #if UNITY_SERVER
 			TickHandler.s_DoTick += CalculateMaps;
-			TickHandler.s_GatherValues += Converter;
+			TickHandler.s_GatherValues += SaveMapToGameState;
 #endif
 		}
 		private void OnDestroy()
 		{
 #if UNITY_SERVER
 			TickHandler.s_DoTick -= CalculateMaps;
-			TickHandler.s_GatherValues -= Converter;
+			TickHandler.s_GatherValues -= SaveMapToGameState;
 #endif
 		}
 
 		void CalculateMaps(int tick)
 		{
+			//----- ----- init ----- -----
 			HeatMapReturnValue value;
+
+			//----- ----- ResourceMap ----- -----
 			value = ResourceMapCalculate.s_instance.RefreshCalcRes();
-			_heatMaps[0] = value.tex;
-			_bitFields[0].FromArray(value.bitfield);
+			_heatMaps[0] = ConvertTexture(value.tex);
+			_bitFields[0] = new BitField2D(value.tex.width, value.tex.height, value.bitfield);
+
+			//----- ----- TerritoriumMap ----- -----
 			value = TerritoriumMapCalculate.s_instance.RefreshCalcTerritorium();
-			_heatMaps[1] = value.tex;
-			_bitFields[1].FromArray(value.bitfield);
+			_heatMaps[1] = ConvertTexture(value.tex);
+			_bitFields[1] = new BitField2D(value.tex.width, value.tex.height, value.bitfield);
 		}
 
-		void Converter(int tick)
+		void SaveMapToGameState(int tick)
 		{
-			foreach(var it in GetAllIds())
+			for(int i = 0; i < _heatMaps.Length; i++)
 			{
 				GSC.heatMap hm = new GSC.heatMap();
-				hm._id = it;
+				hm._id = i;
 
 				//setting up current Bitfield
-				Vector2Int size = GetHeatMapSize(it);
-				hm._mask = GetBitMap(it);
+				hm._mask = _bitFields[i];
 
 				Vector2Int[] positions = hm._mask.GetActiveBits();
 
 				//saving values
-				Texture2D heatMap = GetHeatMap(it);
 
 				hm._values = new List<float>(positions.Length);
-				for(int i = 0; i < positions.Length; i++)
+				for(int j = 0; j < positions.Length; j++)
 				{
-					hm._values.Add(heatMap.GetPixel(positions[i].x, positions[i].y).r);
+					hm._values.Add(_heatMaps[i].GetPixel(positions[j].x, positions[j].y).r);
 				}
 
 				TickHandler.s_interfaceGameState._heatMaps.Add(hm);
@@ -69,48 +73,30 @@ namespace PPBA
 		/// <summary>
 		/// 
 		/// </summary>
-		/// <returns>all ids of existing HeatMaps</returns>
-		public int[] GetAllIds()
-		{
-			int[] value = new int[_heatMaps.Length];
-			for(int i = 0; i < value.Length; i++)
-			{
-				value[i] = i;
-			}
-			return value;
-		}
-
-		/// <summary>
-		/// 
-		/// </summary>
 		/// <param name="id">the id of the heatMap</param>
 		/// <returns>size of the heatMap</returns>
-		public Vector2Int GetHeatMapSize(int id)
-		{
+		public Vector2Int GetHeatMapSize(int id) => new Vector2Int(_heatMaps[id].width, _heatMaps[id].height);
 
-			throw new System.NotImplementedException();
-		}
-
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <param name="id">the id of the heatMap</param>
-		/// <returns>the values that have changed in the last tick</returns>
-		public BitField2D GetBitMap(int id)
+		Texture2D ConvertTexture(RenderTexture rt)
 		{
-			return _bitFields[id];
-		}
+			Texture2D tex = new Texture2D(rt.width, rt.height, TextureFormat.RGB24, false);
 
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <param name="id">the id of the heatMap</param>
-		/// <returns>the texture of the respective heatMap</returns>
-		public Texture2D GetHeatMap(int id)
-		{
-			// pointer auf map
-			throw new System.NotImplementedException();
-			//return _heatMaps[id];
+			// ofc you probably don't have a class that is called CameraController :P
+			Camera activeCamera = Camera.main;
+
+			// Initialize and render
+			activeCamera.targetTexture = rt;
+			activeCamera.Render();
+			RenderTexture.active = rt;
+
+			// Read pixels
+			tex.ReadPixels(new Rect(0, 0, tex.width, tex.height), 0, 0);
+
+			// Clean up
+			activeCamera.targetTexture = null;
+			RenderTexture.active = null; // added to avoid errors 
+			DestroyImmediate(rt);
+			return tex;
 		}
 	}
 }
