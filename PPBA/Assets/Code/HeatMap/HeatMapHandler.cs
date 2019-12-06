6b68827abaa980a6a0057aaa81d7ec6bfc58bba1
[JAM] Andreas <andreas.edmeier@web.de>
2019-12-06 14:23:14 +0100

    [PRO] moved code to HeatMapHandler

--- PPBA/Assets/Code/HeatMap/HeatMapHandler.cs
+++ PPBA/Assets/Code/HeatMap/HeatMapHandler.cs
@@ -6,14 +6,68 @@ namespace PPBA
 {
 	public class HeatMapHandler : Singleton<HeatMapHandler>
 	{
+		RenderTexture[] _heatMaps;
+		BitField2D[] _bitFields;
+
+		private void Start()
+		{
+#if UNITY_SERVER
+			TickHandler.s_DoTick += CalculateMaps;
+			TickHandler.s_GatherValues += Converter;
+#endif
+		}
+		private void OnDestroy()
+		{
+#if UNITY_SERVER
+			TickHandler.s_DoTick -= CalculateMaps;
+			TickHandler.s_GatherValues -= Converter;
+#endif
+		}
+
+		void CalculateMaps(int tick)
+		{
+			ResourceMapCalculate.s_instance.RefreshCalcRes();
+			TerritoriumMapCalculate.s_instance.RefreshCalcTerritorium();
+		}
+
+		void Converter(int tick)
+		{
+			foreach(var it in GetAllIds())
+			{
+				GSC.heatMap hm = new GSC.heatMap();
+				hm._id = it;
+
+				//setting up current Bitfield
+				Vector2Int size = GetHeatMapSize(it);
+				hm._mask = GetBitMap(it);
+
+				Vector2Int[] positions = hm._mask.GetActiveBits();
+
+				//saving values
+				Texture2D heatMap = GetHeatMap(it);
+
+				hm._values = new List<float>(positions.Length);
+				for(int i = 0; i < positions.Length; i++)
+				{
+					hm._values.Add(heatMap.GetPixel(positions[i].x, positions[i].y).r);
+				}
+
+				TickHandler.s_interfaceGameState._heatMaps.Add(hm);
+			}
+		}
+
 		/// <summary>
 		/// 
 		/// </summary>
 		/// <returns>all ids of existing HeatMaps</returns>
 		public int[] GetAllIds()
 		{
-
-			throw new System.NotImplementedException();
+			int[] value = new int[_heatMaps.Length];
+			for(int i = 0; i < value.Length; i++)
+			{
+				value[i] = i;
+			}
+			return value;
 		}
 
 		/// <summary>
@@ -32,11 +86,9 @@ namespace PPBA
 		/// </summary>
 		/// <param name="id">the id of the heatMap</param>
 		/// <returns>the values that have changed in the last tick</returns>
-		public byte[] GetBitMap(int id)
+		public BitField2D GetBitMap(int id)
 		{
-			// out von compute shader 
-
-			throw new System.NotImplementedException();
+			return _bitFields[id];
 		}
 
 		/// <summary>
@@ -48,6 +100,7 @@ namespace PPBA
 		{
 			// pointer auf map
 			throw new System.NotImplementedException();
+			//return _heatMaps[id];
 		}
 	}
 }
