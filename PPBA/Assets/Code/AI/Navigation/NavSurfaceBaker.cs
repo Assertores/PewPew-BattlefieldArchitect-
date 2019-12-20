49e4cc0979fb0956f285ba01acf3b51343064472
teppich <johannesspicker@me.com>
2019-12-20 14:51:20 +0100

    [PRO] behavior value change
    - also nav area registration and baking try but fail

--- PPBA/Assets/Code/AI/Navigation/NavSurfaceBaker.cs
+++ PPBA/Assets/Code/AI/Navigation/NavSurfaceBaker.cs
@@ -5,12 +5,18 @@ using UnityEngine.AI;
 
 namespace PPBA
 {
-	public class NavSurfaceBaker : MonoBehaviour
+	public class NavSurfaceBaker : Singleton<NavSurfaceBaker>
 	{
 		[HideInInspector] public const int _navMask = 127;
 		public static List<NavMeshSurface> _surfaces = new List<NavMeshSurface>();
 		public static bool _isSurfacesDirty = false;//let spawning obstacles set this to true
 		public static bool _isPathsDirty = false;//global variable to let pawns know the NavMesh has changed, they will recalculate before moving
+
+		/*
+		public static List<NavMeshModifier> _navMods = new List<NavMeshModifier>();
+		public static List<NavMeshModifierVolume> _navModVolumes = new List<NavMeshModifierVolume>();
+		public static NavMeshSurface _navSurface;
+		*/
 
 		// Start is called before the first frame update
 		void Start()
@@ -46,19 +52,24 @@ namespace PPBA
 		public void ReadyPathsFlag(int tick = 0)
 		{
 			_isPathsDirty = false;
+			_isSurfacesDirty = true;
 		}
 
 		#region Initialisation
 		private void OnEnable()
 		{
-			//TickHandler.s_Setup += ReadyPathsFlag;//this has to happen before DoInput and after DoTick
+#if UNITY_SERVER
+			TickHandler.s_SetUp += ReadyPathsFlag;//this has to happen before DoInput and after DoTick
 			TickHandler.s_EarlyCalc += ReadyNavMesh;
+#endif
 		}
 
 		private void OnDisable()
 		{
+#if UNITY_SERVER
 			//TickHandler.s_Setup -= ReadyPathsFlag;
 			TickHandler.s_EarlyCalc -= ReadyNavMesh;
+#endif
 		}
 		#endregion
 	}
