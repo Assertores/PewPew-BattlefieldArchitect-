3039601343781b52c47a4f593a36dfb4914dc0c8
Rene <Rene.Gnahs@gmx.de>
2020-01-13 02:10:27 +0100

    [PRO] fix unitScreen

--- PPBA/Assets/Code/UI/UnitScreenSelection.cs
+++ PPBA/Assets/Code/UI/UnitScreenSelection.cs
@@ -11,33 +11,33 @@ public class UnitScreenSelection : MonoBehaviour
 	
 	void OnMouseDown()
 	{
-		string test1 = "Resources " + GetComponent<RefineryRefHolder>()._CurrentResources.ToString();
-		string test2 = "Live " + GetComponent<RefineryRefHolder>()._LivePoints.ToString();
+		//string test1 = "Resources " + GetComponent<RefineryRefHolder>()._CurrentResources.ToString();
+		//string test2 = "Live " + GetComponent<RefineryRefHolder>()._LivePoints.ToString();
 
-		UnitScreenController.s_instance.AddUnitInfoPanel(gameObject.transform, test1 , test2, ref _UnitDetails);
-		UnitScreenUpdate(true);
+		//UnitScreenController.s_instance.AddUnitInfoPanel(gameObject.transform, test1 , test2, ref _UnitDetails);
+		//UnitScreenUpdate(true);
 	}
 
-	public void UnitScreenUpdate( bool UpdateScreen)
-	{
-		if(UpdateScreen == true)
-		{
-			StartCoroutine(UnitScreeenUpdateRoutine());
-		}
-		else
-		{
-			StopCoroutine(UnitScreeenUpdateRoutine());
-		}
-	}
+	//public void UnitScreenUpdate( bool UpdateScreen)
+	//{
+	//	if(UpdateScreen == true)
+	//	{
+	//		StartCoroutine(UnitScreeenUpdateRoutine());
+	//	}
+	//	else
+	//	{
+	//		StopCoroutine(UnitScreeenUpdateRoutine());
+	//	}
+	//}
 
-	IEnumerator UnitScreeenUpdateRoutine()
-	{
-		while(true)
-		{
-			_UnitDetails[0].text = "Resources " + GetComponent<RefineryRefHolder>()._CurrentResources.ToString();
-			_UnitDetails[1].text = "Live " + GetComponent<RefineryRefHolder>()._LivePoints.ToString();
+	//IEnumerator UnitScreeenUpdateRoutine()
+	//{
+	//	while(true)
+	//	{
+	//		_UnitDetails[0].text = "Resources " + GetComponent<RefineryRefHolder>()._CurrentResources.ToString();
+	//		_UnitDetails[1].text = "Live " + GetComponent<RefineryRefHolder>()._LivePoints.ToString();
 
-			yield return new WaitForSeconds(_ScreenRefreshTime);
-		}
-	}
+	//		yield return new WaitForSeconds(_ScreenRefreshTime);
+	//	}
+	//}
 }
