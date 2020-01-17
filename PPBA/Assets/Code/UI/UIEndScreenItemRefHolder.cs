9e8fe28075ac15f2e5f2ebe59ef0d6f76af83da2
[JAM] Andreas <andreas.edmeier@web.de>
2020-01-17 13:07:08 +0100

    [PRO] created EndScreen

---
+++ PPBA/Assets/Code/UI/UIEndScreenItemRefHolder.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace PPBA
+{
+	public class UIEndScreenItemRefHolder : MonoBehaviour
+	{
+		public TextMeshProUGUI _title;
+		public TextMeshProUGUI _aiCount;
+		public TextMeshProUGUI _resources;
+	}
+}
