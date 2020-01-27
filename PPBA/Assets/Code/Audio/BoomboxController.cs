f15f4126aef8feafe5530f9f4b6fafd7024789f4
teppich <johannesspicker@me.com>
2020-01-27 13:57:36 +0100

    [PRO] Ohne Boombox keine blockparty

---
+++ PPBA/Assets/Code/Audio/BoomboxController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PPBA
+{
+	[RequireComponent(typeof(AudioSource))]
+	public class BoomboxController : MonoBehaviour
+	{
+		[SerializeField] private ClipsEnvironment _clipName;
+		private AudioSource _source;
+
+		void Awake()
+		{
+			_source = GetComponent<AudioSource>();
+		}
+
+		void Start()
+		{
+			if(null == _source)
+				return;
+
+			_source.clip = AudioWarehouse.s_instance.Clip(_clipName);
+			_source.Play();
+		}
+
+		void Update()
+		{
+			
+		}
+	}
+}
