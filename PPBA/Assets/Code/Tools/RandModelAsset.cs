bac27c2c58cfec6db823c361bcda25895d864a39
[JAM] Andreas <andreas.edmeier@web.de>
2020-01-28 15:16:49 +0100

    [PRO] randomice Mesh

---
+++ PPBA/Assets/Code/Tools/RandModelAsset.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PPBA
+{
+	[System.Serializable]
+	public struct RanTexElement
+	{
+		public Texture _albedo;
+		public Texture _hight;
+	}
+
+	[CreateAssetMenu(fileName = "RanTexAsset"/*, menuName = "ScriptableObjects/SpawnManagerScriptableObject", order = 1*/)]
+	public class RandModelAsset : ScriptableObject
+	{
+		public Mesh[] _meshs;
+		public RanTexElement[] _ranTexs;
+	}
+}
--- PPBA/Assets/Code/Tools/RandTexture.cs
+++ PPBA/Assets/Code/Tools/RandTexture.cs
@@ -4,34 +4,23 @@ using UnityEngine;
 
 namespace PPBA
 {
-	[System.Serializable]
-	struct RanTexElement
-	{
-		public Texture _albedo;
-		public Texture _hight;
-	}
+
 	public class RandTexture : MonoBehaviour
 	{
-		[SerializeField]RanTex _ranTex;
+		[SerializeField] RandModelAsset _ranTex;
 		void Start()
 		{
-			MeshRenderer[] mr = GetComponentsInChildren<MeshRenderer>();
+			MeshRenderer mr = GetComponent<MeshRenderer>();
+			MeshFilter mf = GetComponent<MeshFilter>();
 
 			int ran = Random.Range(0, _ranTex._ranTexs.Length);
 
-			foreach(var it in mr)
-			{
-				it.material.SetTexture("_MainTex", _ranTex._ranTexs[ran]._albedo);
-				it.material.SetTexture("_ParallaxMap", _ranTex._ranTexs[ran]._hight);
-			}
+			mf.mesh = _ranTex._meshs[Random.Range(0, _ranTex._meshs.Length)];
+
+			mr.material.SetTexture("_MainTex", _ranTex._ranTexs[ran]._albedo);
+			mr.material.SetTexture("_ParallaxMap", _ranTex._ranTexs[ran]._hight);
 
 			Destroy(this);
 		}
-	}
-
-	[CreateAssetMenu(fileName = "RanTexAsset"/*, menuName = "ScriptableObjects/SpawnManagerScriptableObject", order = 1*/)]
-	class RanTex : ScriptableObject
-	{
-		public RanTexElement[] _ranTexs;
 	}
 }
