ae4686d2754fbe540323f6bbbd12761e7ee40243
Rene <Rene.Gnahs@gmx.de>
2020-02-10 01:21:28 +0100

    [PRO] add little FOW, add Extension, (Color ->V4),

---
+++ PPBA/Assets/Code/Tools/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+
+public static class ColorExtensions
+{
+	public static Vector4 ColorToVector4(this Color col )
+	{
+		Vector4 vec = new Vector4(col.r, col.g, col.b, col.a);
+		return vec;
+	}
+
+	public static Vector4[] ColorToVector4(this Color[] col)
+	{
+		Vector4[] vecs = new Vector4[col.Length];
+
+		for(int i = 0; i < col.Length; i++)
+		{
+			vecs[i] = new Vector4(col[i].r, col[i].g, col[i].b, col[i].a);
+		}
+		return vecs;
+	}
+
+}
+
