83344b947488209ff4c950aeac418208b53ea9eb
teppich <johannesspicker@me.com>
2020-02-12 22:01:16 +0100

    [PRO] nav cheats

--- PPBA/Assets/Code/AI/Pawns/Pawn.cs
+++ PPBA/Assets/Code/AI/Pawns/Pawn.cs
@@ -615,9 +615,9 @@ namespace PPBA
 		}
 
 		public Behaviors GetBehaviorsEnum(Behavior behavior) => behavior._name;
-#endregion
+		#endregion
 
-#region Member Admin
+		#region Member Admin
 		public void TakeDamage(int amount)
 		{
 			_health -= amount;
@@ -652,11 +652,13 @@ namespace PPBA
 				_morale += _moraleRegen;
 			}
 		}
-#endregion
+		#endregion
 
-#region Navigation
+		#region Navigation
 		public void NavTick(int tick = 0)//called during DoTick by Execute()
 		{
+			Vector3 originalPos = transform.position;
+
 			if(null == _navMeshPath)
 				_navMeshPath = new NavMeshPath();
 
@@ -699,6 +701,13 @@ namespace PPBA
 
 			if(i - 1 < _navMeshPath.corners.Length)
 				transform.LookAt(new Vector3(_navMeshPath.corners[i - 1].x, transform.position.y, _navMeshPath.corners[i - 1].z));
+
+			if(Vector3.Magnitude(originalPos - _moveTarget) < 1f || Vector3.Magnitude(transform.position - originalPos) < 1f)
+			{
+				NavMeshHit navMeshHit = new NavMeshHit();
+				NavMesh.SamplePosition(transform.position, out navMeshHit, 3f, NavSurfaceBaker._navMask);
+				transform.position = navMeshHit.position;
+			}
 		}
 
 		public void SetMoveTarget(Vector3 newTarget)//call this from the behaviors
@@ -796,9 +805,9 @@ namespace PPBA
 		}
 
 		public void GetBorderData(int tick = 0) => _borderData = HeatMapHandler.s_instance.BorderValues(transform.position);
-#endregion
+		#endregion
 
-#region Physics
+		#region Physics
 		[SerializeField] [Tooltip("Which layers should be used when checking for close objects with CheckOverloadSphere()?")] private LayerMask _overlapSphereLayerMask;
 		private void CheckOverlapSphere()
 		{
@@ -880,15 +889,15 @@ namespace PPBA
 				}
 			}
 		}
-#endregion
+		#endregion
 
-#region Interfaces
+		#region Interfaces
 		private TextMeshProUGUI[] _panelDetails = new TextMeshProUGUI[0];
 		public void InitialiseUnitPanel()
 		{
 			string[] details = new string[] { "Team: " + _team, "Health: " + (int)_health, "Supplies: " + _supplies, "Ammo: " + _ammo, "Morale: " + (int)_morale };
 			UnitScreenController.s_instance.AddUnitInfoPanel(transform, details, ref _panelDetails);
-		    
+
 			if(null != _myBoombox)
 				_myBoombox.PlayClick();
 		}
@@ -909,9 +918,9 @@ namespace PPBA
 				_panelDetails[4].text = "Morale: " + (int)_morale;
 			}
 		}
-#endregion
+		#endregion
 
-#region Gizmos
+		#region Gizmos
 		private void OnDrawGizmos()
 		{
 			/*
@@ -919,9 +928,9 @@ namespace PPBA
 			Gizmos.DrawLine(transform.position, _navMeshPath.corners[_navMeshPath.corners.Length - 1]);//done with a LineRenderer up top
 			*/
 		}
-#endregion
+		#endregion
 
-#region Spawning/Despawning
+		#region Spawning/Despawning
 		private void ClearLists()
 		{
 			_closePawns.Clear();
@@ -1120,6 +1129,6 @@ namespace PPBA
 			TickHandler.s_GatherValues -= WriteToGameState;
 #endif
 		}
-#endregion
+		#endregion
 	}
 }
